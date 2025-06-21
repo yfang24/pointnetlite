a2f65b4cb3f7303fd58489c8f62353ee2b8ed804
Yunping <124202293+yfang24@users.noreply.github.com>
2025-06-21 19:50:47 -0400

    Update get_head.py

--- models/heads/get_head.py
+++ models/heads/get_head.py
@@ -2,12 +2,14 @@ from models.heads.pointnet_cls_head import PointNetClsHead
 from models.heads.pointnet_semseg_head import PointNetSemSegHead
 from models.heads.pointmae_decoder import PointMAEDecoder
 from models.heads.pointmae_cls_head import PointMAEClsHead
+from models.heads.pointnet2_cls_head import PointNet2ClsHead
 
 HEAD_REGISTRY = {
     "pointnet_cls_head": PointNetClsHead,
     "pointnet_semseg_head": PointNetSemSegHead,
     "pointmae_decoder": PointMAEDecoder,
-    "pointmae_cls_head": PointMAEClsHead
+    "pointmae_cls_head": PointMAEClsHead,
+    "pointnet2_cls_head": PointNet2ClsHead
 }
 
 def get_head(config):
