d4e6a1e354e731cf5100f16b1cc5f6bc6d318323
Yunping <124202293+yfang24@users.noreply.github.com>
2025-06-21 20:56:00 -0400

    Update dgcnn_encoder.py

--- models/encoders/dgcnn_encoder.py
+++ models/encoders/dgcnn_encoder.py
@@ -1,1 +1,68 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
+from utils.pcd_utils import knn_gather, group_points
+
+
+class DGCNNEncoder(nn.Module):
+    def __init__(self, k=20, emb_dims=1024):
+        super().__init__()
+        self.k = k
+        self.emb_dims = emb_dims
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(6, 64, kernel_size=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(0.2)
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(64 * 2, 64, kernel_size=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(0.2)
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(64 * 2, 128, kernel_size=1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2)
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(128 * 2, 256, kernel_size=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2)
+        )
+        self.conv5 = nn.Sequential(
+            nn.Conv1d(512, emb_dims, kernel_size=1, bias=False),
+            nn.BatchNorm1d(emb_dims),
+            nn.LeakyReLU(0.2)
+        )
+
+    def _get_graph_feature(self, x, k):
+        """
+        Args:
+            x: (B, N, C)
+        Returns:
+            edge_features: (B, 6, N, k)
+        """
+        idx = knn_gather(x, x, k)               # (B, N, k); for each point, gather its knn neighbors
+        grouped = group_points(x, idx)          # (B, N, k, 3)
+        centers = x.unsqueeze(2)                # (B, N, 1, 3)
+        edge = torch.cat((grouped - centers, centers.expand_as(grouped)), dim=-1)  # (B, N, k, 6) # for each grouped point, set its group coords + group center as features
+        return edge.permute(0, 3, 1, 2).contiguous()  # (B, 6, N, k)
+
+    def forward(self, x):
+        """
+        Args:
+            x: (B, N, 3)
+        Returns:
+            global feature: (B, emb_dims)
+        """
+        x1 = self.conv1(self._get_graph_feature(x, self.k)).max(dim=-1)[0]
+        x2 = self.conv2(self._get_graph_feature(x1.transpose(1, 2), self.k)).max(dim=-1)[0]
+        x3 = self.conv3(self._get_graph_feature(x2.transpose(1, 2), self.k)).max(dim=-1)[0]
+        x4 = self.conv4(self._get_graph_feature(x3.transpose(1, 2), self.k)).max(dim=-1)[0]
+
+        x_cat = torch.cat((x1, x2, x3, x4), dim=1)  # (B, 512, N)
+        x_feat = self.conv5(x_cat)                 # (B, emb_dims, N)
+        x_global = F.adaptive_max_pool1d(x_feat, 1).squeeze(-1)  # (B, emb_dims)
+        return x_global
