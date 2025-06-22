314e81a350b60aeeb70bbf25a6230bdd9b406755
Yunping <124202293+yfang24@users.noreply.github.com>
2025-06-21 21:21:48 -0400

    Update pointnet2_encoder.py

--- models/encoders/pointnet2_encoder.py
+++ models/encoders/pointnet2_encoder.py
@@ -12,13 +12,15 @@ class PointNet2Encoder(nn.Module):
         # Layer configurations
         self.sa1_params = {
             "num_group": 512,
-            "group_size": 32,
+            "group_size": 32 if not use_msg else [16, 32, 128],
+            "radius": 0.2 if not use_msg else [0.1, 0.2, 0.4],
             "mlps": [[64, 64, 128]] if not use_msg else [[32, 32, 64], [64, 64, 128], [64, 96, 128]]
         }
-
+        
         self.sa2_params = {
             "num_group": 128,
-            "group_size": 64,
+            "group_size": 64 if not use_msg else [32, 64, 128],
+            "radius": 0.4 if not use_msg else [0.2, 0.4, 0.8],
             "mlps": [[128, 128, 256]] if not use_msg else [[64, 64, 128], [128, 128, 256], [128, 128, 256]]
         }
 
@@ -31,18 +33,13 @@ class PointNet2Encoder(nn.Module):
         self.sa3_mlp_layers = self._build_mlp_layers(sum(m[-1] for m in self.sa2_params["mlps"]), self.sa3_mlp)
 
     def _build_mlp_blocks(self, mlp_list):
-        blocks = nn.ModuleList()
-        for mlp in mlp_list:
-            blocks.append(self._build_mlp_layers(3, mlp))  # Input = groups (B, 3, S, G)
-        return blocks
-
+        return nn.ModuleList([self._build_mlp_layers(3, mlp) for mlp in mlp_list])
+    
     def _build_mlp_layers(self, in_dim, mlp_channels):
         layers = []
         last_dim = in_dim
         for out_dim in mlp_channels:
-            layers.append(nn.Conv2d(last_dim, out_dim, 1)) # shared mlp over local groups
-            layers.append(nn.BatchNorm2d(out_dim))
-            layers.append(nn.ReLU())
+            layers += [nn.Conv2d(last_dim, out_dim, 1), nn.BatchNorm2d(out_dim), nn.ReLU()] # conv2d: shared mlp over local groups
             last_dim = out_dim
         return nn.Sequential(*layers)
 
@@ -57,11 +54,11 @@ class PointNet2Encoder(nn.Module):
         B, N, _ = x.shape
 
         # SA1
-        neighborhoods, centers = sample_and_group(x, self.sa1_params["num_group"], self.sa1_params["group_size"])
+        neighborhoods, centers = sample_and_group_ball(x, self.sa1_params["num_group"], self.sa1_params["group_size"])
         features = self._apply_mlp_blocks(neighborhoods, self.sa1_mlps)
 
         # SA2
-        neighborhoods, centers = sample_and_group(centers, self.sa2_params["num_group"], self.sa2_params["group_size"])
+        neighborhoods, centers = sample_and_group_ball(centers, self.sa2_params["num_group"], self.sa2_params["group_size"])
         features = self._apply_mlp_blocks(neighborhoods, self.sa2_mlps)
 
         # SA3 (global)
@@ -77,10 +74,5 @@ class PointNet2Encoder(nn.Module):
         mlp_blocks: list of shared MLPs
         returns: (B, C, G)
         """
-        feats_list = []
         x = neighborhoods.permute(0, 3, 2, 1)  # (B, 3, S, G)
-        for mlp in mlp_blocks:
-            f = mlp(x)              # (B, C, S, G)
-            f = torch.max(f, dim=2)[0]  # (B, C, G); max-pooled over groups
-            feats_list.append(f)
-        return torch.cat(feats_list, dim=1)  # (B, sum(C), G)
+        return torch.cat([mlp(x).max(dim=2)[0] for mlp in mlp_blocks], dim=1)  # (B, C_total, G); max-pooled over groups and concatenated across scales
