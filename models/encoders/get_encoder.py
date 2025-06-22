c07aa728a8a958dfef831ff30308e60f98355055
Yunping <124202293+yfang24@users.noreply.github.com>
2025-06-21 21:32:53 -0400

    Update get_encoder.py

--- models/encoders/get_encoder.py
+++ models/encoders/get_encoder.py
@@ -2,12 +2,14 @@ from models.encoders.pointnet_encoder import PointNetEncoder
 from models.encoders.pointnetlite_encoder import PointNetLiteEncoder
 from models.encoders.pointmae_encoder import PointMAEEncoder
 from models.encoders.pointnet2_encoder import PointNet2Encoder
+from models.encoders.dgcnn_encoder import DGCNNEncoder
 
 ENCODER_REGISTRY = {
     "pointnet_encoder": PointNetEncoder,
     "pointnetlite_encoder": PointNetLiteEncoder,
     "pointmae_encoder": PointMAEEncoder,
-    "pointnet2_encoder": PointNet2Encoder
+    "pointnet2_encoder": PointNet2Encoder,
+    "dgcnn_encoder": DGCNNEncoder
 }
 
 def get_encoder(config):
