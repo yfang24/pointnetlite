68d639e538aa5f3420d5b7f7d4529b959ad3c6d8
Yunping <124202293+yfang24@users.noreply.github.com>
2025-06-22 02:37:12 -0400

    Update train_viewpoint.py

--- train_viewpoint.py
+++ train_viewpoint.py
@@ -68,10 +68,10 @@ for epoch in range(epochs):
         verts, faces, labels = verts.float().to(device), faces.long().to(device), labels.long().to(device)
         mesh = Meshes(verts=verts*torch.tensor([[-1, 1, -1]], device=verts.device), faces=faces)
 
-        cam_pos = viewpoint_learner(labels)  # (V, 3)
+        cam_pos = viewpoint_learner(labels)  # (1, V, 3)
 
         v_idx = torch.randint(0, num_views, (1,)).item()
-        pts = mesh_utils.render_mesh_torch(mesh, cam_pos[v_idx], num_points=num_points, device=device)
+        pts = mesh_utils.render_mesh_torch(mesh, cam_pos[0, v_idx], num_points=num_points, device=device)
         pts = pcd_utils.normalize_pcd_tensor(pts)
         pts[:, [0, 2]] *= -1  # (N, 3)
         pcs.append(pts)
