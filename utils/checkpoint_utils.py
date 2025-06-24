817b5e5b830f2d7af723bb9e394d876f9f9362d9
Yunping <124202293+yfang24@users.noreply.github.com>
2025-06-24 00:16:40 -0400

    Update checkpoint_utils.py

--- utils/checkpoint_utils.py
+++ utils/checkpoint_utils.py
@@ -3,11 +3,12 @@ from pathlib import Path
 
 from utils.model_utils import unwrap_model
 
-def save_checkpoint(model, optimizer, scheduler, epoch, best_acc, exp_dir, is_best=False):
+def save_checkpoint(encoder, head, optimizer, scheduler, epoch, best_acc, exp_dir, is_best=False):
     checkpoint = {
-        'model_state_dict': unwrap_model(model).state_dict(),
-        'optimizer_state_dict': unwrap_model(optimizer).state_dict(),
-        'scheduler_state_dict': unwrap_model(scheduler).state_dict(),
+        'encoder': unwrap_model(encoder).state_dict(),
+        'head': unwrap_model(head).state_dict(),
+        'optimizer': unwrap_model(optimizer).state_dict(),
+        'scheduler': unwrap_model(scheduler).state_dict(),
         'epoch': epoch + 1,
         'best_acc': best_acc
     }
@@ -19,12 +20,13 @@ def save_checkpoint(model, optimizer, scheduler, epoch, best_acc, exp_dir, is_be
         best_path = Path(exp_dir) / "checkpoint_best.pth"
         torch.save(checkpoint, best_path)
 
-def load_checkpoint(model, optimizer, scheduler, path, device='cuda'):
+def load_checkpoint(encoder, head, optimizer, scheduler, path, device):
     checkpoint = torch.load(path, map_location=device)
 
-    model.load_state_dict(checkpoint['model_state_dict'])
-    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-    scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
+    encoder.load_state_dict(checkpoint['encoder'])
+    head.load_state_dict(checkpoint['head'])
+    optimizer.load_state_dict(checkpoint['optimizer'])
+    scheduler.load_state_dict(checkpoint['scheduler'])
 
     start_epoch = checkpoint.get('epoch', 0) + 1
     best_acc = checkpoint.get('best_acc', 0.0)
