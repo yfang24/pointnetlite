a063750d3e9cbacac26dfffb7236cb6be3fdc71e
Yunping <124202293+yfang24@users.noreply.github.com>
2025-06-25 00:26:37 -0400

    Update transformer_modules.py

--- models/modules/transformer_modules.py
+++ models/modules/transformer_modules.py
@@ -83,31 +83,3 @@ class TransformerEncoder(nn.Module):
         for _, block in enumerate(self.blocks):
             x = block(x + pos)
         return x
-
-
-class TransformerDecoder(nn.Module):
-    def __init__(self, embed_dim=384, depth=4, num_heads=6, mlp_ratio=4., 
-                 qkv_bias=False, qk_scale=None, drop=0., attn_drop=0., drop_path=0.1):
-        super().__init__()
-        self.blocks = nn.ModuleList([
-            TransformerBlock(embed_dim, num_heads, mlp_ratio, qkv_bias, qk_scale,
-                  drop, attn_drop, drop_path[i] if isinstance(drop_path, list) else drop_path)
-            for i in range(depth)])
-        self.norm = nn.LayerNorm(embed_dim)
-
-        self.apply(self._init_weights)
-
-    def _init_weights(self, m):
-        if isinstance(m, nn.Linear):
-            nn.init.xavier_uniform_(m.weight)
-            if isinstance(m, nn.Linear) and m.bias is not None:
-                nn.init.constant_(m.bias, 0)
-        elif isinstance(m, nn.LayerNorm):
-            nn.init.constant_(m.bias, 0)
-            nn.init.constant_(m.weight, 1.0)
-
-    def forward(self, x, pos, return_token_num):
-        for _, block in enumerate(self.blocks):
-            x = block(x + pos)
-
-        return self.norm(x[:, -return_token_num:]) # only return the mask tokens predict pixel
