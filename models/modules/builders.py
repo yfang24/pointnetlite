f597b118b4eea0303f0b243757d7de65fb499856
Yunping <124202293+yfang24@users.noreply.github.com>
2025-06-25 19:52:18 -0400

    Update builders.py

--- models/modules/builders.py
+++ models/modules/builders.py
@@ -2,17 +2,26 @@ import torch.nn as nn
 
 # shared mlp = conv + bn + act
 # last mlp = conv + bn  (typically)
-def build_shared_mlp(dims, conv_dim=1, act=nn.ReLU(inplace=True)):
+def build_shared_mlp(dims, conv_dim=1, act=nn.ReLU(inplace=True), final_act=False):
     '''
     conv_dim: default=1; set conv_dim=2 if using grouped points (B, G, S, in_dim)
     '''
-    assert conv_dim in [1, 2]
+    assert conv_dim in [1, 2], "conv_dim must be 1 or 2"
     conv = nn.Conv1d if conv_dim == 1 else nn.Conv2d
     bn = nn.BatchNorm1d if conv_dim == 1 else nn.BatchNorm2d
-    
+
     layers = []
-    for in_dim, out_dim in zip(dims[:-1], dims[1:]):
-        layers += [conv(in_dim, out_dim, 1), bn(out_dim), act]
+    n_layers = len(dims) - 1
+
+    for i in range(n_layers):
+        in_dim, out_dim = dims[i], dims[i + 1]
+        is_last = i == n_layers - 1
+
+        layers.append(conv(in_dim, out_dim, 1))
+        layers.append(bn(out_dim))
+        if not is_last or final_act:
+            layers.append(act)
+
     return nn.Sequential(*layers)
 
 # fc_layer = linear + bn + act + dropout
