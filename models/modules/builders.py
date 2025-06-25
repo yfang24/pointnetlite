853936d44a59e5a7851a8b8ec1558c3795efd77e
Yunping <124202293+yfang24@users.noreply.github.com>
2025-06-25 18:59:43 -0400

    Update builders.py

--- models/modules/builders.py
+++ models/modules/builders.py
@@ -1,24 +1,37 @@
 import torch.nn as nn
 
 # shared mlp = conv + bn + act
-def build_shared_mlp(dims, dim=1, act=nn.ReLU(inplace=True)): # set dim=2 if using grouped points (B, G, S, in_dim)
-    assert dim in [1, 2]
-    conv = nn.Conv1d if dim == 1 else nn.Conv2d
-    bn = nn.BatchNorm1d if dim == 1 else nn.BatchNorm2d
+def build_shared_mlp(dims, conv_dim=1, act=nn.ReLU(inplace=True)):
+    '''
+    conv_dim: default=1; set conv_dim=2 if using grouped points (B, G, S, in_dim)
+    '''
+    assert conv_dim in [1, 2]
+    conv = nn.Conv1d if conv_dim == 1 else nn.Conv2d
+    bn = nn.BatchNorm1d if conv_dim == 1 else nn.BatchNorm2d
     
     layers = []
     for in_dim, out_dim in zip(dims[:-1], dims[1:]):
         layers += [conv(in_dim, out_dim, 1), bn(out_dim), get_activation(act)]
     return nn.Sequential(*layers)
 
-# fc = linear + bn + act
-def build_fc_layers(dims, act=nn.ReLU(inplace=True), final_act=False, dropout=0.0)
+# fc = linear + bn + act + dropout
+def build_fc_layers(dims, act=nn.ReLU(inplace=True), final_act=False, dropout=0.0):
+    '''
+    dropout: float or list of float(s), dropout rate(s) per layer
+    '''
     layers = []
+
+    # Normalize dropout to list
+    if isinstance(dropout, (float, int)):
+        dropout = [dropout] * n
+    assert len(dropout) == n, "Dropout length must match number of layers"
+    
     for i in range(len(dims) - 1):
-        layers.append(nn.Linear(dims[i], dims[i + 1]))
-        layers.append(nn.BatchNorm1d(dims[i + 1]))
-        if i < len(dims) - 2 or final_act:
+        layers.append(nn.Linear(dims[i], dims[i+1]))
+        layers.append(nn.BatchNorm1d(dims[i+1]))
+        if final_act or i < n - 1:
             layers.append(act)
-            if dropout > 0:
-                layers.append(nn.Dropout(dropout))
+        if dropout[i] > 0:
+            layers.append(nn.Dropout(dropout[i]))
+    
     return nn.Sequential(*layers)
