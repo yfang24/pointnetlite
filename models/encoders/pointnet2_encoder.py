7395e32eb9c9c585be97a4cf45a03b82d3be0b37
Yunping <124202293+yfang24@users.noreply.github.com>
2025-06-24 23:34:50 -0400

    Update pointnet2_encoder.py

--- models/encoders/pointnet2_encoder.py
+++ models/encoders/pointnet2_encoder.py
@@ -7,6 +7,7 @@ from utils.pcd_utils import fps, ball_group, group_points
 class PointNet2Encoder(nn.Module):
     def __init__(self, in_dim=3, use_msg=False):
         super().__init__()
+        self.use_msg = use_msg
 
         # mlp_layer: conv2d + bn2d + relu
         # mlp: stack of mlp_layers
@@ -19,7 +20,7 @@ class PointNet2Encoder(nn.Module):
             "radius": [0.2] if not use_msg else [0.1, 0.2, 0.4],
             "mlps": [[64, 64, 128]] if not use_msg else [[32, 32, 64], [64, 64, 128], [64, 96, 128]]
         }
-        
+
         self.sa2_params = {
             "num_group": 128,
             "group_size": [64] if not use_msg else [32, 64, 128],
@@ -28,99 +29,52 @@ class PointNet2Encoder(nn.Module):
         }
 
         # last sa: no grouping; a global mlp to aggregate the whole pc
-        self.sa3_params = {
-            "num_group": 1,
-            "group_size": [-1],
-            "radius": [-1],
-            "mlps": [[256, 512, 1024]]
-        }
+        self.sa3_mlp_channels = [256, 512, 1024]
 
         # for msg, sa layer input is the concatenation of features from all msg branches of earlier sa layer
-        self.sa1_mlps = [self._build_mlp(in_dim, sa1_mlp_channels) for sa1_mlp_channels in self.sa1_params["mlps"]]
-        self.sa2_mlps = [self._build_mlp(in_dim + sum(m[-1] for m in self.sa1_params["mlps"]), sa2_mlp_channels) for sa2_mlp_channels in self.sa2_params["mlps"]]
-        self.sa3_mlps = [self._build_mlp(in_dim + sum(m[-1] for m in self.sa2_params["mlps"]), sa3_mlp_channels) for sa3_mlp_channels in self.sa3_params["mlps"]]
-    
+        self.sa1_mlps = [self._build_mlp(in_dim, mlp) for mlp in self.sa1_params["mlps"]]
+        self.sa2_mlps = [self._build_mlp(in_dim + sum(m[-1] for m in self.sa1_params["mlps"]), mlp) for mlp in self.sa2_params["mlps"]]
+        self.sa3_mlp = self._build_mlp(in_dim + sum(m[-1] for m in self.sa2_params["mlps"]), self.sa3_mlp_channels)
+        
     def _build_mlp(self, in_dim, mlp_channels):
         layers = []
-        last_dim = in_dim
         for out_dim in mlp_channels:
-            layers += [nn.Conv2d(last_dim, out_dim, 1), nn.BatchNorm2d(out_dim), nn.ReLU()] # conv2d: shared mlp over local groups
-            last_dim = out_dim
+            layers += [nn.Conv2d(in_dim, out_dim, 1), nn.BatchNorm2d(out_dim), nn.ReLU()] # conv2d: shared mlp over local groups
+            in_dim = out_dim
         return nn.Sequential(*layers)
         
-    def _apply_mlp(self, x, mlp):
-        """
-        x: (B, G, S, C_in)
-        returns: (B, G, C_out)
-        """
-        x = neighborhoods.permute(0, 3, 2, 1)  # (B, C_in, S, G)
-        x = mlp(x).max(dim=2)[0]               # (B, C_out, S, G), max over S -> (B, C_out, G) 
+    def _apply_mlp(self, x, mlp):   # x: neighborhood_points; (B, G, k, C_in)
+        x = neighborhoods.permute(0, 3, 2, 1)  # (B, C_in, k, G)
+        x = mlp(x).max(dim=2)[0]               # (B, C_out, k, G), max over neighborhood k -> (B, C_out, G) 
         return x.permute(0, 2, 1)              # (B, G, C_out)
 
-    def _single_scale_group(self, input_points, input_feats, centers, radius, group_size, mlp):
-        """
-        input_points: (B, G, 3)
-        input_feats: (B, G, D)
-        centers: (B, G, 3)
-        """
-        if radius == -1:   # for last sa (sa3); treat whole pc as one group: num_group G = 1, group_size k = G
-            neighborhood_points = input_points.unsqueeze(1)    # (B, 1, G, 3)
-        else:
-            idx = ball_group(input_points, centers, radius, group_size)  # (B, G, k)
-            neighborhood_points = group_points(input_points, idx) - centers.unsqueeze(2)  # (B, G, k, 3)    
-        if input_feats is not None:
-            if radius == -1:
-                grouped_feats = input_feats.unsqueeze(1)    # (B, 1, G, D)
+    def _sa_layer(self, points, feats, params, mlps):
+        centers = fps(points, params["num_group"])  # (B, G, 3)
+        feat_list = []
+        for r, k, mlp in zip(params["radius"], params["group_size"], mlps):
+            idx = ball_group(points, centers, r, k)  # (B, G, k)
+            neighbor_pts = group_points(points, idx) - centers.unsqueeze(2)  # (B, G, k, 3)
+            if feats is not None:
+                neighbor_feats = group_points(feats, idx)  # (B, G, k, D)
+                group_feats = torch.cat([neighbor_pts, neighbor_feats], dim=-1)  # (B, G, k, 3+D)
             else:
-                grouped_feats = group_points(input_feats, idx)  # (B, G, k, D)
-            neighborhood_feats = torch.cat([neighborhood_points, grouped_feats], dim=-1)  # (B, G, k, 3+D); (B, 1, G, 3+D) for sa3
-        else:   # for sa1
-            neighborhood_feats = neighborhood_points  # (B, G, k, 3)    
-        return self._apply_mlp(neighborhood_feats, mlp)  # group_features; (B, G, C); (B, 1, C) for sa3
+                group_feats = neighbor_pts
+            feat_list.append(self._apply_mlp(group_feats, mlp))  # (B, G, C)
+        return centers, torch.cat(feat_list, dim=2)  # (B, G, sum(C))
 
-    def _sa_layer(self, input_points, input_feats, num_group, radius, group_size, mlps):
-        centers = fps(input_points, num_group)    # (B, G, 3)
-        feats_list = []
-        for r, k, mlp in zip(radius, group_size, mlps):
-            f = self._single_scale_group(input_points, input_feats, centers, r, k, mlp)  # (B, G, C)
-            feats_list.append(f)
-        features = torch.cat(feats_list, dim=2)  # (B, G, sum(C))
-        return centers, features
+    def _global_sa(self, points, feats, mlp):   # treat whole pc as one group, i.e., G = 1, k = N
+        points = points.unsqueeze(1)  # (B, 1, N, 3)
+        if feats is not None:
+            feats = feats.unsqueeze(1)  # (B, 1, N, D)
+            x = torch.cat([points, feats], dim=-1)  # (B, 1, N, 3+D)
+        else:
+            x = points
+        return self._apply_mlp(x, mlp).squeeze(1)  # (B, 1, C) -> (B, C)
 
     def forward(self, x):
-        B, N, _ = x.shape
-
         # SA1
-        centers1, features1 = self._sa_layer(
-            x,
-            None,
-            self.sa1_params["num_group"],
-            self.sa1_params["radius"],
-            self.sa1_params["group_size"],
-            self.sa1_mlps
-        )
-
+        c1, f1 = self._sa_layer(x, None, self.sa1_params, self.sa1_mlps)
         # SA2
-        centers2, features2 = self._sa_layer(
-            centers1,
-            features1,
-            self.sa2_params["num_group"],
-            self.sa2_params["radius"],
-            self.sa2_params["group_size"],
-            self.sa2_mlps
-        )
-        
-        # SA3 (global)    
-        _, features3 = self._sa_layer(
-            centers2,
-            features2,
-            self.sa3_params["num_group"],
-            self.sa3_params["radius"],
-            self.sa3_params["group_size"],
-            self.sa3_mlps
-        )
-        
-        x = features3.squeeze(-1)
-        return x  # (B, 1024)
-
-    
+        c2, f2 = self._sa_layer(c1, f1, self.sa2_params, self.sa2_mlps)
+        # SA3
+        return self._global_sa(c2, f2, self.sa3_mlp)  # (B, 1024)
