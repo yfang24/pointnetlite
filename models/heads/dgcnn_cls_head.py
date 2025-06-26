76060f618f4409f8638935de60f78079a4cd97ad
Yunping <124202293+yfang24@users.noreply.github.com>
2025-06-25 20:49:52 -0400

    Update dgcnn_cls_head.py

--- models/heads/dgcnn_cls_head.py
+++ models/heads/dgcnn_cls_head.py
@@ -1,19 +1,15 @@
 import torch
 import torch.nn as nn
 
+from models.modules.builders import build_fc_layers
+
 class DGCNNClsHead(nn.Module):
     def __init__(self, embed_dim=1024, hidden_dims=[512, 256], out_dim=40, dropout=0.5):
         super().__init__()
-        self.fc1 = nn.Linear(embed_dim*2, hidden_dims[0], bias=False)
-        self.bn1 = nn.BatchNorm1d(hidden_dims[0])
-        self.dp1 = nn.Dropout(dropout)
 
-        self.fc2 = nn.Linear(hidden_dims[0], hidden_dims[1], bias=False)
-        self.bn2 = nn.BatchNorm1d(hidden_dims[1])
-        self.dp2 = nn.Dropout(dropout)
-
-        self.fc3 = nn.Linear(hidden_dims[1], out_dim)
-
+        self.fc = build_fc_layers([embed_dim*2] + hidden_dims, 
+                                  act=nn.LeakyReLU(negative_slope=0.2, inplace=True)), 
+                                  dropout=dropout, final_bn=False, final_act=False)
     def forward(self, x):
         x = F.leaky_relu(self.bn1(self.fc1(x)), negative_slope=0.2)
         x = self.dp1(x)
