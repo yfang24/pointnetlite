c41323714c0d0febd7ebe61d5852dbe799492293
Yunping <124202293+yfang24@users.noreply.github.com>
2025-06-26 13:07:04 -0400

    Update show_rec.py

--- show_rec.py
+++ show_rec.py
@@ -24,8 +24,8 @@ def main(exp_name):
         help="Which checkpoint to use: 'best' (default) or 'last'"
     )
     parser.add_argument(
-        "-cm", "--confusion_matrix", action="store_true",
-        help="Whether to show the confusion matrix"
+        "-vset", "--viz_dataset", type=str, choices=["train", "test"], default="test",
+        help="Which dataset to viz"
     )
     parser.add_argument(
     "-viz", "--viz_reconstruction",
@@ -47,46 +47,21 @@ def main(exp_name):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # Load dataset
-    test_set, rotation_vote, num_votes = get_dataset(config, "test_dataset")
-    test_loader = DataLoader(
-        test_set,
-        batch_size=config.get("batch_size", 32),
-        shuffle=False,
-        num_workers=config.get("num_workers", 4)
-    )
+    if args.viz_dataset == "train"
+        dataset = get_dataset(config, "train_dataset")
+    else:
+        dataset, _, _ = get_dataset(config, "test_dataset")
 
     # Load model
     encoder = get_encoder(config).to(device)
     head = get_head(config).to(device)
-    model = torch.nn.Sequential(encoder, head).to(device)
-    loss_fn = get_loss(config)
-
-    load_checkpoint(model, checkpoint_path, device)
-
-    # Evaluate
-    is_pretrain = config.get("pretrain", False)
-    if is_pretrain:
-        _ = pretrain_evaluate(
-            model, test_loader, loss_fn, device, None, rotation_vote, num_votes
-        )
-    else:
-        _, _, _, _, _, _, test_cm = evaluate(
-            model, test_loader, loss_fn, device, None, rotation_vote, num_votes
-        )
-
-    # Plot confusion matrix
-    if args.confusion_matrix:
-        class_names = getattr(test_set, "class_names", [str(i) for i in range(test_cm.shape[0])])
-        disp = ConfusionMatrixDisplay(confusion_matrix=test_cm, display_labels=class_names)
-        disp.plot(xticks_rotation=45, cmap="Blues")
-        plt.title("Confusion Matrix")
-        plt.tight_layout()
-        plt.savefig(exp_dir / "confusion_matrix.png")
-        plt.show()
-
+    
+    load_checkpoint(ckpt_path, device, encoder=encoder, head=head)
+    
     # Show visible, reconstructed, ground truth pcs
+    
     if args.viz_reconstruction:
-        inv_class_map = {v: k for k, v in test_set.class_map.items()}
+        inv_class_map = {v: k for k, v in dataset.class_map.items()}
         shown = set()            
         num_viz = 5
         viz_pcds = []
@@ -97,31 +72,23 @@ def main(exp_name):
         
         with torch.no_grad():
             for pc, label in test_set:
-                pc = pc.unsqueeze(0).float().to(device)  # (1, N, 3)               
+                if isinstance(pc, tuple):                    
+                    vis_pts, mask_pts, _ = tuple(t.float().to(device) for t in pc)  # (1, N, 3)
+                else:
+                    pc = pc.unsqueeze(0).float().to(device)  # (1, N, 3) 
+                    
                 label = label.item()
                 if label in shown:
                     continue
 
-                x_vis, mask, neighborhood, center = encoder(pc, noaug=False)
-                rec, gt = head(x_vis, mask, neighborhood, center)
-                
-                vis_pts = neighborhood[0][~mask[0]].reshape(-1, 3).cpu().numpy() # (G_visible * S, 3)
+                vis_token, vis_centers = encoder(vis_pts)
+                rec, gt = head(vis_token, vis_centers, mask_pts)
+            
+                vis_pts = vis_pts.reshape(-1, 3).cpu().numpy() # (G_visible * S, 3)
                 rec_pts = rec.reshape(-1, 3).cpu().numpy() # # (G_masked * S, 3)
+                gt_pts = gt.reshape(-1, 3).cpu().numpy()
 
-                gt_pts = neighborhood[0].reshape(-1, 3).cpu().numpy() # (N, 3)
-                gt_colors  = np.tile([0, 153, 255], (gt_pts.shape[0], 1)) # blue = visible
-                masked_indices = np.arange(gt_pts.shape[0])[mask[0].flatten().repeat(neighborhood[0].shape[1])]
-                gt_colors[masked_indices] = [128, 128, 128] # grey = masked
-                pcd_gt = init_pcd(gt_pts, colors=gt_colors)
-
-                rc_pts = np.concatenate([rec_pts, vis_pts], axis=0)
-                rc_colors = np.concatenate([
-                    np.tile([[255, 0, 0]], (rec_pts.shape[0], 1)),    # red = reconstructed
-                    np.tile([[0, 153, 255]], (vis_pts.shape[0], 1))   # blue = visible
-                ])
-                pcd_rc = init_pcd(rc_pts, colors=rc_colors)
-
-                viz_pcds.extend([pcd_gt, pcd_rc])
+                viz_pcds.extend([vis_pts, rec_pts, gt_pts])
                 
                 class_name = inv_class_map[label]
                 viz_class_names.append(class_name)
@@ -132,9 +99,10 @@ def main(exp_name):
                     
         print(f"\n[Visualization Summary]")
         print(f"- Each column is a single object from classes: {viz_class_names}")
-        print(f"- Row 1: Ground truth (visible: blue, masked: gray)")
-        print(f"- Row 2: Reconstructed output (visible: blue, reconstructed: red)\n")
-        viz_pcd(viz_pcds, spacing=2, rows=2)
+        print(f"- Row 1: Visible")
+        print(f"- Row 2: Reconstructed")
+        print(f"- Row 3: Ground Truth")
+        viz_pcd(viz_pcds, spacing=2, rows=3)
         
 if __name__ == "__main__":
     main()
