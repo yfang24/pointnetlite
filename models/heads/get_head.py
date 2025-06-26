e62cc50a20649089cdb43c7ced7e518f8f8c4e46
Yunping <124202293+yfang24@users.noreply.github.com>
2025-06-25 21:26:29 -0400

    Update get_head.py

--- models/heads/get_head.py
+++ models/heads/get_head.py
@@ -2,7 +2,6 @@ from models.heads.pointnet_cls_head import PointNetClsHead
 from models.heads.pointnet_semseg_head import PointNetSemSegHead
 from models.heads.pointmae_decoder import PointMAEDecoder
 from models.heads.pointmae_cls_head import PointMAEClsHead
-from models.heads.pointnet2_cls_head import PointNet2ClsHead
 from models.heads.dgcnn_cls_head import DGCNNClsHead
 
 HEAD_REGISTRY = {
@@ -10,7 +9,6 @@ HEAD_REGISTRY = {
     "pointnet_semseg_head": PointNetSemSegHead,
     "pointmae_decoder": PointMAEDecoder,
     "pointmae_cls_head": PointMAEClsHead,
-    "pointnet2_cls_head": PointNet2ClsHead,
     "dgcnn_cls_head": DGCNNClsHead
 }
 
