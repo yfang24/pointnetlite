0842930b6bf283e31b2b1953017f0b356139b331
Yunping <124202293+yfang24@users.noreply.github.com>
2025-06-26 02:16:18 -0400

    Update modelnet_mae_render.py

--- datasets/modelnet_mae_render.py
+++ datasets/modelnet_mae_render.py
@@ -76,7 +76,8 @@ class ModelNetMAERender(Dataset):
             view_idx = np.random.randint(self.num_views)
             idx = obj_idx * self.num_views + view_idx
 
-        return self.data[idx], self.labels[idx]
+        vis_pts, mask_pts, reflected_pts = self.data[idx]
+        return vis_pts, mask_pts, reflected_pts, self.labels[idx]
 
     def _process_and_render(self):
         data, labels = [], []
