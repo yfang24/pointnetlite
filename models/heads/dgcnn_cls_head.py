f2f9c25fa1a407c8970117ddd97536b186675d9e
Yunping <124202293+yfang24@users.noreply.github.com>
2025-06-25 21:13:14 -0400

    Update dgcnn_cls_head.py

--- models/heads/dgcnn_cls_head.py
+++ models/heads/dgcnn_cls_head.py
@@ -6,12 +6,7 @@ from models.modules.builders import build_fc_layers
 class DGCNNClsHead(nn.Module):
     def __init__(self, embed_dim=1024, hidden_dims=[512, 256], out_dim=40, dropout=0.5):
         super().__init__()
-
-        self.fc = build_fc_layers([embed_dim*2] + hidden_dims, linear_bias=False, act=nn.LeakyReLU(negative_slope=0.2, inplace=True)), dropout=dropout)
+        self.fc = build_fc_layers([embed_dim*2] + hidden_dims + [out_dim], linear_bias=False, act=nn.LeakyReLU(negative_slope=0.2, inplace=True)), dropout=dropout)
+        
     def forward(self, x):
-        x = F.leaky_relu(self.bn1(self.fc1(x)), negative_slope=0.2)
-        x = self.dp1(x)
-        x = F.leaky_relu(self.bn2(self.fc2(x)), negative_slope=0.2)
-        x = self.dp2(x)
-        x = self.fc3(x)
-        return x
+        return self.fc(x)
