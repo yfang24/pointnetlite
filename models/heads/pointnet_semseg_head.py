fd72e8732a2f0c014b6fed236e7ab033e9d0871f
Yunping <124202293+yfang24@users.noreply.github.com>
2025-06-25 21:49:09 -0400

    Update pointnet_semseg_head.py

--- models/heads/pointnet_semseg_head.py
+++ models/heads/pointnet_semseg_head.py
@@ -4,12 +4,19 @@ import torch.nn as nn
 from models.modules.builders import build_fc_layers
 
 class PointNetSemSegHead(nn.Module):
-    def __init__(self, embed_dim=1024 + 64, hidden_dims=[512, 256, 128], out_dim=13, dropout=0.):
+    def __init__(self, embed_dim=1024, local_dim=64, hidden_dims=[512, 256, 128], out_dim=13, dropout=0.):
         super().__init__()
-        self.fc = build_fc_layers([embed_dim] + hidden_dims + [out_dim], linear_bias=True, act=nn.ReLU(inplace=True), dropout=dropout)
+        self.out_dim = out_dim
+        self.fc = build_fc_layers([embed_dim + local_dim] + hidden_dims + [out_dim], linear_bias=True, act=nn.ReLU(inplace=True), dropout=dropout)
 
-    def forward(self, x):  # x: (B, C, N)
-        B, _, N = x.size()
-        x = self.fc(x)
-        x = x.transpose(2, 1).contiguous()  # (B, N, out_dim)
-        return x.view(-1, self.out_dim)
+    def forward(self, x):
+        global_feat, local_feat = x   # (B, embed_dim), (B, N, local_dim)
+        
+        B, N, _ = local_feat.shape
+        global_expanded = global_feat.unsqueeze(1).expand(-1, N, -1)  # (B, N, embed_dim)
+        x = torch.cat([global_expanded, local_feat], dim=-1)         # (B, N, embed_dim + local_dim)
+        
+        x = x.permute(0, 2, 1)                                        # (B, C, N)
+        x = self.fc(x)                                                # (B, out_dim, N)
+        x = x.transpose(2, 1).contiguous()                            # (B, N, out_dim)
+        return x.view(-1, self.out_dim)                               # (B * N, out_dim)       
