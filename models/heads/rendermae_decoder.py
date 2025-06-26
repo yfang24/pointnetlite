e370ebf845e1cbab0877a882fcc4f4b6f93187d5
Yunping <124202293+yfang24@users.noreply.github.com>
2025-06-26 04:01:02 -0400

    Update rendermae_decoder.py

--- models/heads/rendermae_decoder.py
+++ models/heads/rendermae_decoder.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 
 from models.modules.transformer_modules import TransformerEncoder
 from models.modules.builders import build_shared_mlp
+from utils.pcd_utils import fps, knn_group, group_points
 
 class RenderMAEDecoder(nn.Module):
     def __init__(self, embed_dim=384, depth=4, drop_path=0.1, num_heads=6, out_dim=3):
@@ -28,34 +29,54 @@ class RenderMAEDecoder(nn.Module):
         
         self.rec_head = build_shared_mlp([embed_dim, 256, 128, out_dim], conv_dim=1)
 
-    # def forward(self, vis_token, vis_pts, reflected_pts):
+    # def forward(self, vis_token, vis_pts, reflected_pts):        
+    #     """
+    #     Args:
+    #         vis_token: (B, N_visible, D) - encoded visible tokens
+    #         vis_pts: (B, N_visible, 3) - input visible points (for position encoding)
+    #         reflected_pts: (B, N_mask, 3) - reflected visible points (for mask position encoding)
+    #     Returns:
+    #         pred_pts: (B, N_mask, 3) - reconstructed masked points
+    #     """
+    #     B, N_mask, _ = reflected_pts.shape
+    #     D = vis_token.shape[-1]
+
+    #     vis_pos = self.pos_embed(vis_pts)             # (B, N_visible, D)
+    #     mask_pos = self.pos_embed(reflected_pts)      # (B, N_mask, D)        
+    #     mask_token = self.mask_token.expand(B, N_mask, D)  # (B, N_mask, D)
+        
+    #     x_full = torch.cat([vis_token, mask_token], dim=1)   # (B, N_all, D)
+    #     pos_full = torch.cat([vis_pos, mask_pos], dim=1)     # (B, N_all, D)
+
+    #     x = self.blocks(x_full, pos_full)                    # (B, N_all, D)
+    #     x = self.norm(x)
+
+    #     pred_mask = x[:, -N_mask:, :]                        # (B, N_mask, D)
+    #     pred_mask = pred_mask.transpose(1, 2)                # (B, D, N_mask)
+    #     pred_pts = self.rec_head(pred_mask).transpose(1, 2)  # (B, N_mask, 3)
+
+    #     return pred_pts
+        return pred_pts, mask_pts
+        
     def forward(self, x):
-        vis_token, vis_pts, mask_pts, reflected_pts = x
-        
-        """
-        Args:
-            vis_token: (B, N_visible, D) - encoded visible tokens
-            vis_pts: (B, N_visible, 3) - input visible points (for position encoding)
-            reflected_pts: (B, N_mask, 3) - reflected visible points (for mask position encoding)
-        Returns:
-            pred_pts: (B, N_mask, 3) - reconstructed masked points
-        """
-        B, N_mask, _ = reflected_pts.shape
-        D = vis_token.shape[-1]
+        vis_token, vis_centers, mask_pts, reflected_pts = x
 
-        vis_pos = self.pos_embed(vis_pts)             # (B, N_visible, D)
-        mask_pos = self.pos_embed(reflected_pts)      # (B, N_mask, D)        
-        mask_token = self.mask_token.expand(B, N_mask, D)  # (B, N_mask, D)
-        
-        x_full = torch.cat([vis_token, mask_token], dim=1)   # (B, N_all, D)
-        pos_full = torch.cat([vis_pos, mask_pos], dim=1)     # (B, N_all, D)
+        B, G, D = vis_token.shape
 
-        x = self.blocks(x_full, pos_full)                    # (B, N_all, D)
+        gt_centers = fps(mask_pts, G)
+        mask_centers = fps(reflected_pts, G)
+        
+        vis_pos = self.pos_embed(vis_centers)        
+        mask_pos = self.pos_embed(mask_centers)
+        pos_full = torch.cat([vis_pos, mask_pos], dim=1)
+
+        mask_token = self.mask_token.expand(B, G, D)
+        x_full = torch.cat([vis_token, mask_token], dim=1)
+
+        x = self.blocks(x_full, pos_full)
         x = self.norm(x)
 
-        pred_mask = x[:, -N_mask:, :]                        # (B, N_mask, D)
-        pred_mask = pred_mask.transpose(1, 2)                # (B, D, N_mask)
-        pred_pts = self.rec_head(pred_mask).transpose(1, 2)  # (B, N_mask, 3)
-
-        # return pred_pts
-        return pred_pts, mask_pts
+        pred_mask = x[:, -G:, :]
+        pred_mask = pred_mask.transpose(1, 2)
+        pred_centers = self.rec_head(pred_mask).transpose(1, 2)
+        return pred_centers, gt_centers
