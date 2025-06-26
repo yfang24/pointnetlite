f7760b3cb1f064c72ed0d9d3c939cdfe9cd752e3
Yunping <124202293+yfang24@users.noreply.github.com>
2025-06-25 21:45:51 -0400

    Update pointnet_semseg_head.py

--- models/heads/pointnet_semseg_head.py
+++ models/heads/pointnet_semseg_head.py
@@ -4,24 +4,12 @@ import torch.nn as nn
 from models.modules.builders import build_fc_layers
 
 class PointNetSemSegHead(nn.Module):
-    def __init__(self, embed_dim=1024 + 64, hidden_dims=[512, 256, 128], out_dim=13):
+    def __init__(self, embed_dim=1024 + 64, hidden_dims=[512, 256, 128], out_dim=13, dropout=0.):
         super().__init__()
         self.fc = build_fc_layers([embed_dim] + hidden_dims + [out_dim], linear_bias=True, act=nn.ReLU(inplace=True), dropout=dropout)
-        self.conv1 = nn.Conv1d(embed_dim, 512, 1)
-        self.conv2 = nn.Conv1d(512, 256, 1)
-        self.conv3 = nn.Conv1d(256, 128, 1)
-        self.conv4 = nn.Conv1d(128, out_dim, 1)
-
-        self.bn1 = nn.BatchNorm1d(512)
-        self.bn2 = nn.BatchNorm1d(256)
-        self.bn3 = nn.BatchNorm1d(128)
-        self.out_dim = out_dim
 
     def forward(self, x):  # x: (B, C, N)
         B, _, N = x.size()
-        x = F.relu(self.bn1(self.conv1(x)))
-        x = F.relu(self.bn2(self.conv2(x)))
-        x = F.relu(self.bn3(self.conv3(x)))
-        x = self.conv4(x)
+        x = self.fc(x)
         x = x.transpose(2, 1).contiguous()  # (B, N, out_dim)
         return x.view(-1, self.out_dim)
